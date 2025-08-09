c0159665d030d5d75bab668b318f45412508c395
Sijawusz Pur Rahnama <sija@sija.pl>
2025-08-09 14:14:24 +0200

    Add some doc comments

--- src/portfolio/asset.cr
+++ src/portfolio/asset.cr
@@ -2,11 +2,13 @@ module Portfolio
   record Asset, amount : BigDecimal, currency : Money::Currency, description : String? = nil do
     include YAML::Serializable
 
+    # Returns value of this asset as a `Money` object made from `amount` and `currency`.
     @[YAML::Field(ignore: true)]
     getter value : Money do
       Money.from_amount(amount, currency)
     end
 
+    # Returns `value` exchanged to `Money.default_currency`.
     @[YAML::Field(ignore: true)]
     getter exchanged : Money do
       value.exchange_to(Money.default_currency)
--- src/portfolio/config.cr
+++ src/portfolio/config.cr
@@ -2,14 +2,21 @@ module Portfolio
   class Config
     include YAML::Serializable
 
+    # Rate provider used for currency exchange.
     @[YAML::Field(converter: Money::Currency::RateProvider::Converter)]
     getter rate_provider : Money::Currency::RateProvider
 
+    # Time to live (TTL) for currency rates, `nil` means unlimited TTL.
     @[YAML::Field(converter: Time::Span::Converter)]
     getter currency_rates_ttl : Time::Span?
+
+    # Currency used for asset values.
     getter currency : Money::Currency
+
+    # Array of assets to track.
     getter assets : Array(Asset)
 
+    # Total value of `assets` represented in `currency`.
     def assets_total : Money
       assets.sum(&.exchanged)
     end
