18d1bf3885c003a60a5392b24ec6751d69811f83
Sijawusz Pur Rahnama <sija@sija.pl>
2025-08-10 16:43:40 +0200

    Move responsibilities regarding currency rate cache management into the `Config` class

--- src/cli.cr
+++ src/cli.cr
@@ -4,9 +4,6 @@ require "./portfolio"
 
 Log.setup_from_env
 
-currency_rates_filepath =
-  Path[Dir.tempdir] / "portfolio" / ".cache" / "currency_rates.json"
-
 clear_currency_rates_cache = false
 config_path = nil
 
@@ -33,7 +30,7 @@ end
 option_parser.parse
 
 if clear_currency_rates_cache
-  File.delete?(currency_rates_filepath)
+  Portfolio::Config.clear_currency_rates_cache!
 end
 
 unless config_path
@@ -45,8 +42,6 @@ unless config_path
   end
 end
 
-Dir.mkdir_p(currency_rates_filepath.dirname)
-
 # ameba:disable Lint/NotNil
 config = File.open(config_path.not_nil!) do |file|
   Portfolio::Config.from_yaml(file)
@@ -54,11 +49,7 @@ end
 
 Money.configure do |context|
   context.default_currency = config.currency
-  context.default_rate_store =
-    Money::Currency::RateStore::File.new(
-      filepath: currency_rates_filepath,
-      ttl: config.currency_rates_ttl,
-    )
+  context.default_rate_store = config.rate_store
   context.default_rate_provider = config.rate_provider
 end
 
--- src/portfolio/config.cr
+++ src/portfolio/config.cr
@@ -2,6 +2,29 @@ module Portfolio
   class Config
     include YAML::Serializable
 
+    # Filepath to currency rates cache.
+    CURRENCY_RATES_FILEPATH =
+      Path[Dir.tempdir] / "portfolio" / ".cache" / "currency_rates.json"
+
+    # Initializes currency rates cache.
+    def self.init_currency_rates_cache! : Nil
+      Dir.mkdir_p(CURRENCY_RATES_FILEPATH.dirname)
+    end
+
+    # Clears currency rates cache.
+    def self.clear_currency_rates_cache! : Bool
+      File.delete?(CURRENCY_RATES_FILEPATH)
+    end
+
+    # Currency rate store to use.
+    @[YAML::Field(ignore: true)]
+    getter rate_store : Money::Currency::RateStore do
+      Money::Currency::RateStore::File.new(
+        filepath: CURRENCY_RATES_FILEPATH,
+        ttl: currency_rates_ttl,
+      )
+    end
+
     # Rate provider used for currency exchange.
     @[YAML::Field(converter: Money::Currency::RateProvider::Converter)]
     getter rate_provider : Money::Currency::RateProvider
@@ -19,6 +42,10 @@ module Portfolio
     # Total value of `assets` represented in `currency`.
     def assets_total : Money
       assets.sum(&.exchanged)
+    end
+
+    private def after_initialize
+      Config.init_currency_rates_cache!
     end
   end
 end
