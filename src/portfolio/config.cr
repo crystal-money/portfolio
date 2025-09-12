40dd9791cbdce2932d8f4eeb1a919b45897593b2
Sijawusz Pur Rahnama <sija@sija.pl>
2025-09-12 21:52:45 +0200

    Make `Config` mutable

--- src/portfolio/config.cr
+++ src/portfolio/config.cr
@@ -4,22 +4,22 @@ module Portfolio
 
     # Rate store used for currency exchange.
     @[YAML::Field(converter: Money::Currency::RateStore::Converter)]
-    getter rate_store : Money::Currency::RateStore do
+    property rate_store : Money::Currency::RateStore do
       Money.default_rate_store
     end
 
     # Rate provider used for currency exchange.
     @[YAML::Field(converter: Money::Currency::RateProvider::Converter)]
-    getter rate_provider : Money::Currency::RateProvider
+    property rate_provider : Money::Currency::RateProvider
 
     # Currency used for asset values.
-    getter currency : Money::Currency
+    property currency : Money::Currency
 
     # Name of the portfolio.
-    getter name : String?
+    property name : String?
 
     # Array of assets to track.
-    getter assets : Array(Asset)
+    property assets : Array(Asset)
 
     # Total value of `assets` represented in `currency`.
     def assets_total : Money
